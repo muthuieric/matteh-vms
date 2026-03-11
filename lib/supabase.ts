b4d3dd243228291091329298066f1bf6e9efcf6d
eric <ericmuthuipatch22@gmail.com>
2026-03-11 13:22:51 +0300

    feat: initial commit

---
+++ lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// This creates a single, reusable connection to your database
+export const supabase = createClient(supabaseUrl, supabaseKey);
